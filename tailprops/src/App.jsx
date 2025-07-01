7bdd593c826d45eba0378fb882276e0af46d5988
Chetan Chaudhari <cbchaudhari22@gmail.com>
2025-07-01 18:25:12 +0530

    tailwind demo

---
+++ tailprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Cards'
+
+function App() {
+
+
+
+  return (
+    <><Card username="chetan"/>
+    <Card username="yash"/>
+    </>
+    
+
+  )
+}
+
+export default App
